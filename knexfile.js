8e958dbb3980159f0aaad68b7122072af66a1d13
yuki <momo_968_weekend@yahoo.co.jp>
2024-08-18 16:28:29 +0900

    knexfile変更

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,20 @@ require('dotenv').config();
 
 module.exports = {
   // 本当はdev(mysql)とprod(pg)で分けたい
-  // development: {
-  //   client: "mysql",
-  //   connection: {
-  //     host: process.env.DB_HOST,
-  //     user: process.env.DB_USER,
-  //     password: process.env.DB_PASSWORD,
-  //     database: process.env.DB_NAME,
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  // },
   development: {
+    client: "mysql",
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+    },
+    pool: {
+      min: 2,
+      max: 10,
+    },
+  },
+  production: {
     client: "pg",
     connection: {
       host: process.env.PGHOST,
@@ -29,18 +29,4 @@ module.exports = {
       max: 10,
     },
   },
-  // production: {
-  //   client: "pg",
-  //   connection: {
-  //     host: process.env.PGHOST,
-  //     user: process.env.PGUSER,
-  //     password: process.env.PGPASSWORD,
-  //     database: process.env.PGDATABASE,
-  //     ssl: { rejectUnauthorized: false }
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10,
-  //   },
-  // },
 };
