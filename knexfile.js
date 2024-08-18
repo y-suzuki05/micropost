1a4ca276e6c38c665319f24b33ff24cf4d161bf9
yuki <momo_968_weekend@yahoo.co.jp>
2024-08-18 15:41:37 +0900

    knexfileコメント

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,20 @@
 require('dotenv').config();
 
 module.exports = {
+  // 本当はdevとprodで分けたい
+  // development: {
+  //   client: "mysql",
+  //   connection: {
+  //     host: process.env.DB_HOST,
+  //     user: process.env.DB_USER,
+  //     password: process.env.DB_PASSWORD,
+  //     database: process.env.DB_NAME,
+  //   },
+  //   pool: {
+  //     min: 2,
+  //     max: 10,
+  //   },
+  // },
   development: {
     client: "pg",
     connection: {
