427cf61ff80e37de5062e9801cf50ff4eb7511bc
yuki <momo_968_weekend@yahoo.co.jp>
2024-08-18 15:49:00 +0900

    knexfile変更

--- knexfile.js
+++ knexfile.js
@@ -15,20 +15,6 @@ module.exports = {
   //     max: 10,
   //   },
   // },
-  development: {
-    client: "pg",
-    connection: {
-      host: process.env.PGHOST,
-      user: process.env.PGUSER,
-      password: process.env.PGPASSWORD,
-      database: process.env.PGDATABASE,
-      ssl: { rejectUnauthorized: false }
-    },
-    pool: {
-      min: 2,
-      max: 10,
-    },
-  },
   production: {
     client: "pg",
     connection: {
